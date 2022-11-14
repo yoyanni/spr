41d4dc5f2b670c316b1f85550a5467a975d0cb3b
yoyanni <yanni.alevras@gmail.com>
2022-11-14 08:09:35 +0200

    Create human choice and create game logic

--- script.js
+++ script.js
@@ -1,8 +1,51 @@
+// Start Game
+
+function startGame(playerSelection, computerSelection) {
+  //annouce winner (1 round)
+  switch (true) {
+    case playerSelection == computerChoice:
+      console.log("It's a draw!");
+      break;
+    case playerSelection == "scissors" && computerChoice == "paper":
+      console.log("You win!");
+      break;
+    case playerSelection == "scissors" && computerChoice == "rock":
+      console.log("You lose!");
+      break;
+    case playerSelection == "paper" && computerChoice == "rock":
+      console.log("You win!");
+      break;
+    case playerSelection == "paper" && computerChoice == "scissors":
+      console.log("You lose!");
+      break;
+    case playerSelection == "rock" && computerChoice == "scissors":
+      console.log("You win!");
+      break;
+    case playerSelection == "rock" && computerChoice == "paper":
+      console.log("You lose!");
+      break;
+    default:
+      console.log("Something went wrong.. please try again.");
+  }
+}
+
+// Player Choice
+function getPlayerChoice() {
+  let choice = prompt(
+    "The choice of your weapon? \n Scissors, Paper or Rock?"
+  ).toLowerCase();
+  if (choice == "scissors" || choice == "paper" || choice == "rock") {
+    return choice;
+  } else {
+    alert("Please add a valid answer!");
+  }
+}
+
 // Computer Choice
 function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3);
   let choices = ["Scissors", "Paper", "Rock"];
-  console.log(choices[choice]);
+  return choices[choice].toLowerCase();
 }
 
-getComputerChoice();
+startGame(getPlayerChoice(), getComputerChoice());
